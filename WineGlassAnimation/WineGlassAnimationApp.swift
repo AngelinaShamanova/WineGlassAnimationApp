b3a8caf6bed524c64f1e0eb380fa2e9d19c055ef
AngelinaShamanova <shamanovaaaa@yandex.ru>
2023-02-07 17:00:34 +0100

    Initial Commit

---
+++ WineGlassAnimation/WineGlassAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineGlassAnimationApp.swift
+//  WineGlassAnimation
+//
+//  Created by Ангелина Шаманова on 7.2.23..
+//
+
+import SwiftUI
+
+@main
+struct WineGlassAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
